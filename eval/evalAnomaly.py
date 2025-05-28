7c75456a7501406bc82b4f6a6ce4b29c03230cff
LUCA BERGAMINI s332167 <s332167@studenti.polito.it>
2025-05-28 11:25:29 +0200

    generalization fix

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -65,21 +65,24 @@ def main():
     #model_file = importlib.import_module(args.loadModel[:-3])
     #model = ERFNet(NUM_CLASSES)
     
-    # Add the `train/` directory to sys.path
-    train_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "train"))
-    if train_dir not in sys.path:
-        sys.path.insert(0, train_dir)
-    
-    model_path = args.loadModel
-    model_name = "bisenet"
+    if os.path.splitext(os.path.basename(args.loadModel))[0] == "bisenet":
+        # Add the `train/` directory to sys.path
+        train_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "train"))
+        if train_dir not in sys.path:
+            sys.path.insert(0, train_dir)
+        
+        model_path = args.loadModel
+        model_name = "bisenet"
 
-    if not os.path.isabs(model_path):
-        # Convert to absolute path relative to current working directory
-        model_path = os.path.abspath(model_path)
+        if not os.path.isabs(model_path):
+            # Convert to absolute path relative to current working directory
+            model_path = os.path.abspath(model_path)
 
-    spec = importlib.util.spec_from_file_location(model_name, model_path)
-    model_file = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(model_file)
+        spec = importlib.util.spec_from_file_location(model_name, model_path)
+        model_file = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(model_file)
+    else:
+        model_file = importlib.import_module(args.loadModel[:-3])
     
     model = model_file.Net(NUM_CLASSES)
 
