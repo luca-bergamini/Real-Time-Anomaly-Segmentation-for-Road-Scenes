9fca3d2f9be6829a10afdee2ff8da9168da31cd4
LucaRabellino <126524586+LucaRabellino@users.noreply.github.com>
2025-05-30 19:29:16 +0200

    fix

--- eval/eval_iou.py
+++ eval/eval_iou.py
@@ -71,6 +71,20 @@ def main(args):
         spec = importlib.util.spec_from_file_location(model_name, model_path)
         model_file = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(model_file)
+    elif os.path.splitext(os.path.basename(args.loadModel))[0] == "enet":
+        # Add the `train/` directory to sys.path
+        train_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "train"))
+        if train_dir not in sys.path:
+            sys.path.insert(0, train_dir)
+        model_name = "enet"
+
+        if not os.path.isabs(model_path):
+            # Convert to absolute path relative to current working directory
+            model_path = os.path.abspath(model_path)
+
+        spec = importlib.util.spec_from_file_location(model_name, model_path)
+        model_file = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(model_file)
     else:
         model_file = importlib.import_module(args.loadModel[:-3])
         
